90fb9f031b22bf21219ca21db59cbc1d080d3861
Richard Nguyen <richard@richardhnguyen.com>
2023-08-25 19:56:12 -0700

    feat: add authentication service
    - Add ASPNET core app as the main stack
    - Set up a simple controller for returning mock users
    - Create a User model

---
+++ auth/Models/User.cs
@@ -0,0 +1,15 @@
+namespace auth.Models;
+
+public class User
+{
+    public required string UserName { get; set; }
+
+
+    public required string Password { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+}
