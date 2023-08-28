5de6c5bce1dc55c37fe967083502a06ad151da91
Richard Nguyen <richard@richardhnguyen.com>
2023-08-28 16:03:12 -0700

    feat(gateway): Add unit tests for updating user queries
    - Add test case for updating a user
    - Add test case for updating a non-existing user

--- gateway/src/graphql/resolvers/user/__tests__/userMutation.ts
+++ gateway/src/graphql/resolvers/user/__tests__/userMutation.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { ApolloServer } from "@apollo/server";
 
 import schema from "@graphql/schema";
@@ -98,18 +100,78 @@ describe("mutation Users()", () => {
 
     expect(res.body.kind).toEqual("single");
 
-    // TODO: fix use any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((res.body as any).singleResult.data).toBeNull();
 
-    // TODO: fix use any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect(Array.isArray((res.body as any).singleResult.errors)).toBe(true);
 
-    // TODO: fix use any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     expect((res.body as any).singleResult.errors[0].message).toEqual(
       `User already exists: ${newUser.name}`
+    );
+  });
+
+  it("should update a user", async () => {
+    const query = `#graphql
+      mutation MyUpdateUserQuery($updateUserId: ID!, $input: UpdateUserInput!) {
+          updateUser(id: $updateUserId, input: $input) {
+          id
+          name
+          company
+        }
+      }
+    `;
+
+    const res = await server.executeOperation<typeof mock>({
+      query,
+      variables: {
+        updateUserId: "64ea5312a4b6e4e9e0b29f5c",
+        input: {
+          company: "Costco",
+        },
+      },
+    });
+
+    const expected = {
+      id: "64ea5312a4b6e4e9e0b29f5c",
+      name: "Ratliff Tanner",
+      company: "Costco",
+    };
+
+    expect(res.body.kind).toEqual("single");
+
+    expect((res.body as any).singleResult.data).toBeTruthy();
+
+    expect((res.body as any).singleResult.data.updateUser).toEqual(expected);
+  });
+
+  it("should not update a user if not existing", async () => {
+    const query = `#graphql
+      mutation MyUpdateUserQuery($updateUserId: ID!, $input: UpdateUserInput!) {
+        updateUser(id: $updateUserId, input: $input) {
+          id
+          name
+          company
+        }
+      }
+    `;
+
+    const res = await server.executeOperation<typeof mock>({
+      query,
+      variables: {
+        updateUserId: "64ea5312a4b6e4e9e0b29f5cd",
+        input: {
+          company: "Costco",
+        },
+      },
+    });
+
+    expect(res.body.kind).toEqual("single");
+
+    expect((res.body as any).singleResult.data).toBeNull();
+
+    expect(Array.isArray((res.body as any).singleResult.errors)).toBe(true);
+
+    expect((res.body as any).singleResult.errors[0].message).toEqual(
+      "User not found"
     );
   });
 
