85937aedb7265c74598b479189667eb97c9b1d1c
Richard Nguyen <richard@richardhnguyen.com>
2023-08-29 10:19:16 -0700

    feat(webapp): Add layout and children for <Header />
    - Add label to return to home page
    - Add <HeaderLink /> for navigation
    - Add page navigators
    - Add @primer/octicons-react for common icon usage

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,9 +1,52 @@
 import * as React from "react";
+import clsx from "classnames";
+import Link from "next/link";
+import { MarkGithubIcon } from "@primer/octicons-react";
+import HeaderLink from "./link";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <h1>.Todo</h1>
+    <header
+      aria-description="header"
+      className={clsx("", {
+        "shadow-lg drop-shadow-lg shadow-slate-300": true,
+      })}
+    >
+      <div
+        aria-description="header-wrapper"
+        className={clsx("", {
+          "flex items-center justify-between": true,
+          "w-full md:w-[768px] lg:w-[1024px] mx-auto py-4": true,
+        })}
+      >
+        {/* eslint-disable-next-line jsx-a11y/role-supports-aria-props */}
+        <h1
+          aria-description="header-label"
+          className={clsx("font-mono", {
+            "text-2xl font-bold": true,
+            "md:text-3xl": true,
+            "lg:font-extrabold": true,
+          })}
+        >
+          <HeaderLink href="/">Todo</HeaderLink>
+        </h1>
+
+        <div className="flex items-center text-lg">
+          <div className="flex gap-4">
+            <HeaderLink href="/login">Login</HeaderLink>
+            <HeaderLink href="/register">Register</HeaderLink>
+          </div>
+          <div
+            className={clsx("flex", {
+              "ml-4 pl-4 border-l border-slate-300": true,
+            })}
+          >
+            <HeaderLink href="https://github.com/richardnguyen99/todo">
+              <MarkGithubIcon size={24} />
+            </HeaderLink>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
