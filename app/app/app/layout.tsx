51665ea375c3f2995b4f45f5c74c75c15aa37d29
Richard Nguyen <richard@richardhnguyen.com>
2023-09-09 09:40:00 -0700

    feat(webapp): Add Link List menu for app page

--- app/app/app/layout.tsx
+++ app/app/app/layout.tsx
@@ -1,5 +1,32 @@
+"use client";
+
 import * as React from "react";
 import clsx from "classnames";
+import { CalendarIcon, FilterIcon, RepoIcon } from "@primer/octicons-react";
+
+import AppLink from "./components/app-link";
+import type { AppLinkProps } from "./components/app-link";
+
+const menuData: AppLinkProps[] = [
+  {
+    icon: () => <RepoIcon />,
+    label: "Inbox",
+    to: "/app",
+    number: 0,
+  },
+  {
+    icon: () => <CalendarIcon />,
+    label: "Today",
+    to: "/app/today",
+    number: 0,
+  },
+  {
+    icon: () => <FilterIcon />,
+    label: "Labels",
+    to: "/app/labels",
+    number: 0,
+  },
+];
 
 const AuthAppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
@@ -11,20 +38,21 @@ const AuthAppLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
           "fixed top-12 left-0 bottom-0 z-50": true,
         })}
       >
-        <div className="mt-12 mx-4 flex flex-col w-full">
-          <ul className="flex flex-col">
-            <li>Inbox</li>
-            <li>Today</li>
-            <li>Upcoming</li>
-            <li>Filters & Labeling</li>
+        <div className="mt-12 ml-2 mr-2 flex flex-col w-full">
+          <ul className="flex flex-col gap-1;">
+            {menuData.map((item, i) => (
+              <AppLink key={i} {...item} />
+            ))}
           </ul>
-          <div className="mt-8 pt-8 border-t border-indigo-200">
-            <h3>Add projects +</h3>
-            <ul className="flex flex-col mt-2 ml-4">
-              <li>Project 1</li>
-              <li>Work</li>
-              <li>School</li>
-            </ul>
+          <div className="mt-8 pt-8 px-3 mx-3 border-t border-indigo-200">
+            <div className="-mx-3">
+              <h3>Add projects +</h3>
+              <ul className="flex flex-col mt-2 ml-4">
+                <li>Project 1</li>
+                <li>Work</li>
+                <li>School</li>
+              </ul>
+            </div>
           </div>
           <div className="flex flex-col justify-end h-full my-6">
             <h1>Add workspace</h1>
--- app/app/app/page.tsx
+++ app/app/app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
+import {
+  KebabHorizontalIcon,
+  MultiSelectIcon,
+  ProjectIcon,
+} from "@primer/octicons-react";
 import * as React from "react";
 
 const AuthApp = () => {
   return (
     <div>
-      <h1>Auth App</h1>
+      <div className="flex items-center w-full">
+        <h1 className="text-2xl font-bold">All tasks</h1>
+        <div className="ml-auto flex items-center text-sm gap-4">
+          <div className="flex items-center gap-2 px-2 py-1 rounded-md hover:bg-neutral-200">
+            <ProjectIcon size={16} />
+            <p>Views</p>
+          </div>
+          <div className="flex items-center gap-2 px-2 py-1 rounded-md hover:bg-neutral-200">
+            <MultiSelectIcon size={16} />
+            <p>Filter</p>
+          </div>
+          <div className="flex items-center gap-2 px-1.5 py-1.5 rounded-md hover:bg-neutral-200">
+            <KebabHorizontalIcon size={16} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
